c4de8bce585844bdb544d4eee754c976faf9bdd0
Zlatan Radovanovic <zlatan.radovanovic@fet.ba>
2020-09-25 23:44:27 +0200

    Revert "drm/msm/dsi-staging: update dsi clock calculations"
    This reverts commit 9c6317b23ea35825883bdb44612403311837b8e0.

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -4391,57 +4391,6 @@ void dsi_panel_put_mode(struct dsi_display_mode *mode)
 	kfree(mode->priv_info);
 }
 
-void dsi_panel_calc_dsi_transfer_time(struct dsi_host_common_cfg *config,
-			struct dsi_mode_info *timing)
-{
-	u32 frame_time_us, nslices;
-	u64 min_bitclk, total_active_pixels, bits_per_line,
-		dsi_transfer_time_us;
-	struct msm_display_dsc_info *dsc = timing->dsc;
-
-	/* Packet overlead in bits,2 bytes header + 2 bytes checksum
-	 * + 1 byte dcs data command.
-	 */
-	const u32 packet_overhead = 56;
-
-	/* Default time between pingpong done to TE in microsecs */
-	const u32 max_tx_threshold_time = 2166;
-
-	frame_time_us = mult_frac(1000, 1000, (timing->refresh_rate));
-
-	if (timing->dsc_enabled) {
-		nslices = (timing->h_active)/(dsc->slice_width);
-		/* (slice width x bit-per-pixel + packet overhead) x
-		 * number of slices x height x fps / lane
-		 */
-		bits_per_line = ((dsc->slice_width * dsc->bpp) +
-				packet_overhead) * nslices;
-		bits_per_line = bits_per_line / (config->num_data_lanes);
-
-		min_bitclk = (bits_per_line * timing->v_active *
-					timing->refresh_rate);
-	} else {
-		total_active_pixels = ((DSI_H_ACTIVE_DSC(timing)
-					* timing->v_active));
-		/* calculate the actual bitclk needed to transfer the frame */
-		min_bitclk = (total_active_pixels * (timing->refresh_rate) *
-				(config->bpp));
-		do_div(min_bitclk, config->num_data_lanes);
-	}
-
-	timing->min_dsi_clk_hz = min_bitclk;
-
-	if (timing->clk_rate_hz) {
-		/* adjust the transfer time proportionately for bit clk*/
-		dsi_transfer_time_us = frame_time_us * min_bitclk;
-		do_div(dsi_transfer_time_us, timing->clk_rate_hz);
-		timing->dsi_transfer_time_us = dsi_transfer_time_us;
-	} else {
-		timing->dsi_transfer_time_us = frame_time_us -
-				max_tx_threshold_time;
-	}
-}
-
 int dsi_panel_get_mode(struct dsi_panel *panel,
 			u32 index, struct dsi_display_mode *mode,
 			int topology_override)
