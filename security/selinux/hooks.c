49f194a6265ca0ef5386cf830d58c3bdd20f650a
Zlatan Radovanovic <zlatan.radovanovic@fet.ba>
2020-10-10 13:21:14 +0200

    Revert "selinux: don't require auditing"
    This reverts commit 2af407259a7d8c7ea3114320988ac4bbdf700894.

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -3152,7 +3152,6 @@ static noinline int audit_inode_permission(struct inode *inode,
 					   int result,
 					   unsigned flags)
 {
-#ifdef CONFIG_AUDIT
 	struct common_audit_data ad;
 	struct inode_security_struct *isec = inode->i_security;
 	int rc;
@@ -3165,7 +3164,6 @@ static noinline int audit_inode_permission(struct inode *inode,
 			    audited, denied, result, &ad, flags);
 	if (rc)
 		return rc;
-#endif
 	return 0;
 }
 
